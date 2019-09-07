682bfd18de5ae64a5929c1a1c9810149b8e765d6
Ferisef <39487012+Ferisef@users.noreply.github.com>
2019-09-07 20:29:52 +0300

    Update clicker.js

--- clicker.js
+++ clicker.js
@@ -1,11 +1,8 @@
 target = document.querySelector('.community-points-summary .tw-z-above .tw-absolute .tw-transition');
 
 observer = new MutationObserver((ms, obs) => {
-  console.log('> new mutations');
 
   for (let m of ms) {
-    console.log('> new mutation', m.addedNodes);
-
     const b = target.querySelector('button');
     if (b) {
      b.click();
