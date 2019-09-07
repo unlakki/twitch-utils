6eb665b39fa885233f8b757ae392ed7647b8f03b
Ferisef <39487012+Ferisef@users.noreply.github.com>
2019-09-08 00:48:28 +0300

    Update clicker.js

--- clicker.js
+++ clicker.js
@@ -1,15 +1,18 @@
 const target = document.querySelector('.community-points-summary .tw-z-above .tw-absolute .tw-transition');
 
-const observer = new MutationObserver((mutations, obs) => {
-
+const observer = new MutationObserver((mutations) => {
   for (let mutation of mutations) {
-    console.log('> mutation', JSON.stringify(mutation));
+    console.log(`> mutation ${mutation.type}`);
 
     const button = target.querySelector('button');
     if (button) {
+      console.log('> coins collected');
       button.click();
     }
   }
 });
 
-observer.observe(target, { attributes: true, childList: true, subtree: true });
+observer.observe(target, { childList: true, subtree: true });
+
+// if you want to stop automatic coin collection, use the command below (without double slashes):
+// observer.disconnect();
