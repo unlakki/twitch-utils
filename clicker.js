3a2732a27e932fe3b987416d7044e6ade39c00f3
Ferisef <39487012+Ferisef@users.noreply.github.com>
2019-09-07 19:51:01 +0300

    Update clicker.js

--- clicker.js
+++ clicker.js
@@ -1,7 +1,16 @@
 const target = document.querySelector('.community-points-summary .tw-z-above .tw-absolute .tw-transition');
-const observer = new MutationObserver((ms, callback) => {
-  for (let m of ms) {
-    if (m.type === 'childList') target.querySelector('button').click();
+
+const observer = new MutationObserver((mutations) => {
+  console.log('> new mutations');
+
+  for (let mutation of mutations) {
+    console.log('> new mutation', mutation);
+
+    const button = target.querySelector('button');
+    if (button) {
+     button.click();
+    }
   }
 });
+
 observer.observe(target, { childList: true });
